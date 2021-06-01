86d36b0fc22bd827fafe93983bf7ea77fb4c106d
IonSibRe <ionsibre@gmail.com>
2021-06-01 22:40:46 +0200

    Finished

--- main.c
+++ main.c
@@ -1,31 +1,48 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
-#include <ctype.h>
 
 // Deklarace funkce 
 char* encrypt(char plaintext[], int key);
+char* decrypt(char ciphertext[], int key);
 
 int main(void)
 {
     // Init
-    char plaintext[1000];
-    int key;
+    char plaintext[1000], ciphertext[1000];
+    int key, choice;
 
-    // Plaintext
-    printf("Plaintext: (Max. 999) ");
-    fgets(plaintext, 1000, stdin); // fgets pro moznost limitovat pocet charakteru pri inputu 
+    // Volba pouziti
+    printf("Sifrace | Desifrace (0 | 1): ");
+    scanf("%d", &choice);
 
-    // Klic pro posun
+    // Klíč
     printf("Key: ");
     scanf("%d", &key);
 
-    // Sifrace
-    printf("Ciphertext: ");
-    printf("%s", encrypt(plaintext, key));
+    if (choice == 0) {
+        // Plaintext
+        printf("Plaintext: (Max. 999) ");
+        scanf(" %999[^\n]", plaintext);
 
-    return 0;
+        // Šifrace
+        printf("Ciphertext: ");
+        printf("%s", encrypt(plaintext, key)); 
+    } 
+    
+    else if (choice == 1) {
+        // Ciphertext
+        printf("Ciphertext: (Max. 999) ");
+        scanf(" %999[^\n]", ciphertext);
+
+        // Dešifrace
+        printf("Plaintext: ");
+        printf("%s", decrypt(ciphertext, key)); 
+    } 
+    
+    else {
+        printf("Je nutno zadat pro volbu 0 | 1");
+        return 1;
+    }
 }
 
 char* encrypt(char plaintext[], int key) {
@@ -33,7 +50,7 @@ char* encrypt(char plaintext[], int key) {
     {
         if (plaintext[i] >= 'a' && plaintext[i] <= 'z')
         {
-            plaintext[i] = 'a' + (plaintext[i] - 'a' + key) % 26;
+            plaintext[i] = 'a' + (plaintext[i] - 'a' + key) % 26; 
         }
         else if (plaintext[i] >= 'A' && plaintext[i] <= 'Z')
         {
@@ -42,4 +59,20 @@ char* encrypt(char plaintext[], int key) {
     }
 
     return plaintext;
+}
+
+char* decrypt(char ciphertext[], int key) {
+     for (int i = 0; i < strlen(ciphertext); i++) // Iteruje skrze plaintext
+    {
+        if (ciphertext[i] >= 'a' && ciphertext[i] <= 'z')
+        {
+            ciphertext[i] = 'z' - ('z' - ciphertext[i] + key) % 26;
+        }
+        else if (ciphertext[i] >= 'A' && ciphertext[i] <= 'Z')
+        {
+            ciphertext[i] = 'Z' - ('Z' - ciphertext[i] + key) % 26;
+        }
+    }
+
+    return ciphertext;
 }
